375b12075a5a4dcdbfb108f9281847452087b4c9
SAMBRANO <joao.sambrano@atos.net>
2021-10-21 08:16:14 -0300

    20-10

---
+++ Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+
+namespace Aula_20_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int valor;  //F9 realiza um breakpoint um debug - f10 executa linha a linha
+
+            //Console.WriteLine("Digite um valor: ");
+            //valor = int.Parse(Console.ReadLine());
+
+            //if(valor > 0)
+            //{
+            //    Console.WriteLine("Valor é positivo");
+            //}
+            //else  if (valor < 0)
+            //{
+            //    Console.WriteLine("O numero não é positivo");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("é zero");
+            //}
+
+            //EXERCICIO 1 - 2
+            //int valor, valor1;
+            //valor = 5;
+
+            //Console.WriteLine("Digite um valor: ");
+            //valor1 = int.Parse(Console.ReadLine());
+
+            //if (valor == valor1)
+            //{
+            //    Console.WriteLine("Valor iguais");
+            //}
+            //else if(valor > valor1)
+            //{
+            //    Console.WriteLine("menor");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Maior");
+            //}
+
+
+            //EXERCICIO 3
+
+            //double n1, n2;
+            //double n3;
+
+            //Console.Write("Insira um numero 1: ");
+            //n1 = double.Parse(Console.ReadLine());
+
+            //Console.Write("Insira um numero 2: ");
+            //n2 = double.Parse(Console.ReadLine());
+
+            //Console.Write("insira um numero de 1=soma,2=sub,3=mult e 4=div ");
+            //n3 = double.Parse(Console.ReadLine());
+
+            //if (n3 == 1)
+            //{
+            //    Console.Write("Resultado da soma : " + (n1 + n2));
+            //}
+            //else if (n3 == 2)
+            //{
+            //    Console.Write("Resultado da subtração : " + (n1 - n2));
+            //}
+            //else if (n3 == 3)
+            //{
+            //    Console.Write("Resultado da Multiplicação : " + (n1 * n2));
+            //}
+            //else if (n3 == 4)
+            //{
+            //    Console.Write("Resultado da divisão : " + (n1 / n2));
+            //}
+            //else{
+            //    Console.Write("INVALIDA!");
+            //}
+
+
+            //Console.ReadKey();
+
+            //EXERCICIO 4 
+
+            //double salario;
+            //double aumento;
+
+            //Console.WriteLine("Informe o salário: ");
+            //salario = double.Parse(Console.ReadLine());
+
+
+            //if (salario > 0 && salario <= 900)
+            //{
+            //    aumento = 5;
+            //}
+            //else if (salario >900 && salario <=1400)
+            //{
+            //    aumento = 8;
+
+            //}
+            //else if (salario > 1400)
+            //{
+            //    aumento = 10;
+            //}
+            //else
+            //{
+            //    Console.WriteLine("novo salario: " + salario);
+            //    aumento = 0;
+            //}
+
+            //salario = salario + salario * (aumento / 100);
+
+            //Console.WriteLine("Novo salario: " + salario);
+
+            //EXERCICIO 5 
+
+            //int valor;
+
+            //Console.WriteLine("Informe o Valor: ");
+            //valor = int.Parse(Console.ReadLine());
+
+            //if (valor % 2 == 0) //impar
+            //{
+            //    Console.WriteLine("O valor é par!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("O Valor é impar");
+            //}
+
+            //EXERCICIO 6 
+
+            //double n1, n2, n3;
+
+            //Console.WriteLine("Informe a medida 1 : ");
+            //n1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Informe a medida 2 : ");
+            //n2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Informe a medida 3:  ");
+            //n3 = int.Parse(Console.ReadLine());
+
+            //if (n1 < (n2 + n3) && n2 < (n1 + n3) && n3 < (n1 + n2))
+            //{
+            //    if (n1 == n2 && n2 == n3 && n3 == n1)
+            //    {
+            //        Console.WriteLine("Equilatero");
+            //    }
+            //    else if (n1 == n2 || n2 == n3 || n3 ==n1)
+            //    {
+            //        Console.WriteLine("Isóceles");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Escaleno");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Não é um triângulo!");
+            //}
+
+            //EXERCICIO 8 //
+
+            //int idade;
+
+            //Console.WriteLine("Qual sua idade : ");
+            //idade = int.Parse(Console.ReadLine());
+
+            //if (idade >= 18 && idade <= 67)
+            //{
+            //    Console.WriteLine("Doação de sangue pode ser efetuada");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Não é possivel doar sangue");
+            //}
+        }
+    }
+}
